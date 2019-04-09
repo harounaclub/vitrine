ed941a70763e1ffb7fce372548010b49dc71595c
sysadmin Host <isplabo@gmail.com>
2019-04-09 03:14:20 +0000

    change live value

--- work_tree/includes/env_properties/live.php
+++ work_tree/includes/env_properties/live.php
@@ -1,11 +1,29 @@
 <?php
 
-$BASE_URL       = "http://istjobs.com/";
+
+//configuration générale
+$BASE_URL       = "http://vitrine.gloohost.net/";
 $ENVIRONMENT="development";
+
+
+//configuration mysql
 
 $DB_HOST  	= "localhost";
 $DB_USER  	= "root";
 $DB_PASSWORD    = "Harouna123##@";
-$DATABASE  	= "mickr_recrutement";
+$DATABASE  	= "qt_apn";
+
+
+//configuration mongo
+
+$DB_MONGO_HOST  	= "localhost";
+$DB_MONGO_PORT  	= "27017";
+$DB_MONGO_USER  	= "";
+$DB_MONGO_PASSWORD    = "";
+$MONGO_DATABASE  	= "qteck_prixkdo";
+
+
+
 
 ?>
+
